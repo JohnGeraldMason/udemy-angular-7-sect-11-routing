9fc747c6c3cbac20a777276386e841d45c3464e0
JohnnyMatrixxx <JohnnyMatrixxx@users.noreply.github.com>
2019-02-05 18:25:27 -0400

    Completed fetching route params reactively + route observables

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -8,6 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,5 +18,16 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    this.route.params
+      .subscribe(
+        (params:Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      )
+  }
+
+  ngOnDestry() {
+    this.paramsSubscription.unsubscribe();
   }
 }
