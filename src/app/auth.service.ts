612cd6e684343f98a0eef3556c99047e77965111
JohnnyMatrixxx <JohnnyMatrixxx@users.noreply.github.com>
2019-02-07 16:10:54 -0400

    Added login and logout buttons.

---
+++ src/app/auth.service.ts
@@ -0,0 +1,25 @@
+// this is fake service for the tutorial
+
+export class AuthService {
+    loggedIn = false;
+  
+    isAuthenticated() {
+      const promise = new Promise(
+        (resolve, reject) => {
+          setTimeout(() => {
+            resolve(this.loggedIn);
+          }, 800);
+        }
+      );
+      return promise;
+    }
+  
+    login() {
+      this.loggedIn = true;
+    }
+  
+    logout() {
+      this.loggedIn = false;
+    }
+  }
+  
